b28d4f294a2621e7a85bbf59558d9d8663b28f90
fuadhasan08 <fuadhasandipro08@gmail.com>
2023-05-25 20:52:43 +0600

    table responsive

--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -20,51 +20,59 @@ const TeachersList = () => {
   }, []);
 
   return (
-    <div className='px-2 lg:px-0 flex-1 lg:col-span-8 mt-10 lg:mt-0'>
+    <div className='px-2 lg:px-0 flex-1 lg:col-span-8 mt-10 mb-5 lg:mt-0'>
       <PageTitle title='শিক্ষকবৃন্দের নামের তালিকা' />
-      <table className='w-full border-collapse'>
-        <thead>
-          <tr className='text-left'>
-            <th className='px-4 py-2 border border-gray-200'>ক্রমিক নং</th>
-            <th className='px-4 py-2 border border-gray-200'>নাম</th>
-            <th className='px-4 py-2 border border-gray-200'>পদবি</th>
-            <th className='px-4 py-2 border border-gray-200'>বিভাগ</th>
-            <th className='px-4 py-2 border border-gray-200'>মোবাইল নাম্বার</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teachers.map((teacher, index) => (
-            <tr
-              key={index}
-              className={`${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'} `}
-            >
-              <td className='px-4 py-2 border border-gray-200'>{index + 1}</td>
-              <td className='px-4 py-2 border border-gray-200'>
-                {teacher.title}
-              </td>
-              <td className='px-4 py-2 border border-gray-200'>
-                {teacher.designation}
-              </td>
-              <td className='px-4 py-2 border border-gray-200'>
-                {teacher.subject}
-              </td>
-              <td className='px-4 py-2 border border-gray-200 items-center inline-flex justify-between w-full'>
-                <a href={`tel:${teacher.phone_number}`}>
-                  {teacher.phone_number}
-                </a>
-                <CopyToClipboard text={teacher.phone_number}>
-                  <button
-                    className='ml-2 focus:outline-none hover:text-primary'
-                    title='Copy to Clipboard'
-                  >
-                    <MdContentCopy size={20} />
-                  </button>
-                </CopyToClipboard>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className='overflow-x-auto'>
+        <div className='min-w-[850px] lg:min-w-[991px] xl:min-w-full'>
+          <table className='w-full border-collapse'>
+            <thead>
+              <tr className='text-left'>
+                <th className='px-4 py-2 border border-gray-200'>ক্রমিক নং</th>
+                <th className='px-4 py-2 border border-gray-200'>নাম</th>
+                <th className='px-4 py-2 border border-gray-200'>পদবি</th>
+                <th className='px-4 py-2 border border-gray-200'>বিভাগ</th>
+                <th className='px-4 py-2 border border-gray-200'>
+                  মোবাইল নাম্বার
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {teachers.map((teacher, index) => (
+                <tr
+                  key={index}
+                  className={`${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'} `}
+                >
+                  <td className='px-4 py-2 border border-gray-200'>
+                    {index + 1}
+                  </td>
+                  <td className='px-4 py-2 border border-gray-200'>
+                    {teacher.title}
+                  </td>
+                  <td className='px-4 py-2 border border-gray-200'>
+                    {teacher.designation}
+                  </td>
+                  <td className='px-4 py-2 border border-gray-200'>
+                    {teacher.subject}
+                  </td>
+                  <td className='px-4 py-2 border border-gray-200 items-center inline-flex justify-between w-full'>
+                    <a href={`tel:${teacher.phone_number}`}>
+                      {teacher.phone_number}
+                    </a>
+                    <CopyToClipboard text={teacher.phone_number}>
+                      <button
+                        className='ml-2 focus:outline-none hover:text-primary'
+                        title='Copy to Clipboard'
+                      >
+                        <MdContentCopy size={20} />
+                      </button>
+                    </CopyToClipboard>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
