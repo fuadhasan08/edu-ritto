839cd32e51ce11503d56689949d7d844b32735ca
fuadhasan08 <fuadhasandipro08@gmail.com>
2023-05-27 14:29:28 +0600

    pagination styled

--- src/pages/Notice.jsx
+++ src/pages/Notice.jsx
@@ -6,9 +6,9 @@ import PageTitle from '../components/common/PageTitle';
 const NoticeList = () => {
   const [notices, setNotices] = useState([]); // Array to store fetched data
   const [sortBy, setSortBy] = useState('date'); // Default sort by date
-  const [sortOrder, setSortOrder] = useState('asc'); // Default sort order
+  const [sortOrder, setSortOrder] = useState('dsc'); // Default sort order
   const [currentPage, setCurrentPage] = useState(1); // Current page number
-  const [noticesPerPage] = useState(20); // Number of notices per page
+  const [noticesPerPage] = useState(15); // Number of notices per page
   const [selectedMonth, setSelectedMonth] = useState(
     `${new Date().getMonth() + 1}`
   ); // Selected month for filtering
@@ -21,7 +21,7 @@ const NoticeList = () => {
     axios
       .get(`${import.meta.env.VITE_API_URI}wp-json/ritto/v1/notices`)
       .then((response) => {
-        setNotices(response.data.sort((a, b) => a.id - b.id));
+        setNotices(response.data);
       })
       .catch((error) => {
         console.error('Error fetching teacher data:', error);
@@ -47,8 +47,8 @@ const NoticeList = () => {
 
     if (sortBy === 'date') {
       sorted.sort((a, b) => {
-        const dateA = new Date(a.notice?.modified);
-        const dateB = new Date(b.notice?.modified);
+        const dateA = new Date(a.id);
+        const dateB = new Date(b.id);
 
         if (sortOrder === 'asc') {
           return dateA - dateB;
@@ -107,60 +107,64 @@ const NoticeList = () => {
     return `${day} ${month}, ${year}`;
   };
 
+  const paginationArray = new Array(
+    Math.floor(notices.length / noticesPerPage)
+  ).fill('');
+
   return (
     <div className='px-2 lg:px-0 flex-1 lg:col-span-8 mt-10 mb-5  lg:mt-0'>
       <PageTitle title='নোটিশ বোর্ড' />
+      <div className='flex justify-end my-4 gap-x-2'>
+        <select
+          value={selectedYear}
+          onChange={(e) => setSelectedYear(e.target.value)}
+          className='px-4 py-3 bg-gray-100 focus:outline-none'
+        >
+          <option value='' disabled>
+            Select Year
+          </option>
+          <option value='2022'>2022</option>
+          <option value='2023'>2023</option>
+          <option value='2024'>2024</option>
+        </select>
+
+        <select
+          value={selectedMonth}
+          onChange={(e) => setSelectedMonth(e.target.value)}
+          className='px-4 py-3 bg-gray-100 focus:outline-none'
+        >
+          <option value='' disabled>
+            Select Month
+          </option>
+          <option value='1'>January</option>
+          <option value='2'>February</option>
+          <option value='3'>March</option>
+          <option value='4'>April</option>
+          <option value='5'>May</option>
+          <option value='6'>June</option>
+          <option value='7'>July</option>
+          <option value='8'>August</option>
+          <option value='9'>September</option>
+          <option value='10'>October</option>
+          <option value='11'>November</option>
+          <option value='12'>December</option>
+        </select>
+        <button
+          onClick={() => sortBySpecificDate(selectedMonth, selectedYear)}
+          className='bg-secondary px-5 py-3 hover:bg-primary transition-all duration-150 hover:text-white'
+        >
+          Filter
+        </button>
+      </div>
       <div className='overflow-x-auto'>
         <div className='min-w-[850px] lg:min-w-[991px] xl:min-w-full'>
-          <div>
-            <select
-              value={selectedYear}
-              onChange={(e) => setSelectedYear(e.target.value)}
-            >
-              <option value='' disabled>
-                Select Year
-              </option>
-              <option value='2022'>2022</option>
-              <option value='2023'>2023</option>
-              <option value='2024'>2024</option>
-            </select>
-
-            <select
-              value={selectedMonth}
-              onChange={(e) => setSelectedMonth(e.target.value)}
-            >
-              <option value='' disabled>
-                Select Month
-              </option>
-              <option value='1'>January</option>
-              <option value='2'>February</option>
-              <option value='3'>March</option>
-              <option value='4'>April</option>
-              <option value='5'>May</option>
-              <option value='6'>June</option>
-              <option value='7'>July</option>
-              <option value='8'>August</option>
-              <option value='9'>September</option>
-              <option value='10'>October</option>
-              <option value='11'>November</option>
-              <option value='12'>December</option>
-            </select>
-            <button
-              onClick={() => sortBySpecificDate(selectedMonth, selectedYear)}
-            >
-              Filter
-            </button>
-          </div>
-
           <table className='w-full border-collapse'>
             <thead>
               <tr className='text-left'>
-                <th className='px-2 py-2 border border-gray-200'> নং</th>
+                <th className='px-4 py-2 border border-gray-200'> নং</th>
                 <th className='px-4 py-2 border border-gray-200'>শিরোনাম</th>
                 <th
-                  className={`min-w-[150px] px-4 py-2 border border-gray-200 cursor-pointer ${
-                    sortBy === 'date' ? 'font-bold' : ''
-                  }`}
+                  className='min-w-[150px] px-4 py-2 border border-gray-200 cursor-pointer'
                   onClick={sortByDate}
                 >
                   তারিখ {sortOrder === 'asc' ? '↑' : '↓'}
@@ -194,25 +198,42 @@ const NoticeList = () => {
               ))}
             </tbody>
           </table>
-
-          <div>
-            {/* Pagination */}
-            <p>CurrPage:{currentPage}</p>
+          <div></div>
+        </div>
+      </div>
+      {/* Pagination */}
+      {paginationArray.length !== 0 && (
+        <div className='flex items-center justify-center lg:my-5 my-8'>
+          <nav className='flex'>
             <button
               onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
               disabled={currentPage === 1}
+              className='px-4 py-3 bg-gray-100 border-r border-gray-400'
             >
-              Previous
+              <span className='text-gray-600'>Previous</span>
             </button>
+            {paginationArray.map((item, idx) => {
+              return (
+                <p
+                  className={`px-4 py-3 bg-gray-100 border-r border-gray-400 ${
+                    currentPage === idx + 1 ? 'bg-primary text-white' : ''
+                  }`}
+                  key={idx}
+                >
+                  <span>{idx + 1}</span>
+                </p>
+              );
+            })}
             <button
               onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
               disabled={indexOfLastNotice >= sortedNotices().length}
+              className='px-4 py-3 bg-gray-100'
             >
-              Next
+              <span className='text-gray-600'>Next</span>
             </button>
-          </div>
+          </nav>
         </div>
-      </div>
+      )}
     </div>
   );
 };
